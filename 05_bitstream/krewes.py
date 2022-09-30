7799a5b02859b36d986025231186f2cb1bf0aeb4
Ravindra Mangar <73133247+RavindraM23@users.noreply.github.com>
2022-09-30 12:42:19 -0400

    Update krewes.py

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -18,32 +18,38 @@ QCC:
 
 import random
 
-dictionary = {}
+krewes = {}
 #Create a new dictionary
 
-with open("krewes.txt", "r") as f:
-  f = f.read()
-  #Read file, turn it into string
-  f = f.replace("\n","")
-  #Replace blank strings
-  f = f.split("@@@")
-  #Split devos
-  for item in f:
-    item = item.split("$$$")
-    #Split between entities for each devo
-    keys = list(dictionary.keys())
-    #Current keys within the dictionary
-    key = item[0]
-    #key takes the value of the period of each devo
-    item.pop(0)
-    #This is the key of the dictionary soo...we don't need this anymore
-    if keys.count(key) == 0:
-      dictionary[key] = [item]
-    #If period of devo is not created already, make it.
-    else:
-      dictionary[key].append(item)
-    #Adds devo name + ducky name in list form to dictionary
+try:
 
+  with open("krewes.txt", "r") as f:
+    f = f.read()
+    #Read file, turn it into string
+    f = f.replace("\n","")
+    #Replace blank strings
+    f = f.split("@@@")
+    #Split devos
+    for item in f:
+      item = item.split("$$$")
+      #Split between entities for each devo
+      keys = list(krewes.keys())
+      #Current keys within the dictionary
+      key = item[0]
+      #key takes the value of the period of each devo
+      item.pop(0)
+      #This is the key of the dictionary soo...we don't need this anymore
+      if keys.count(key) == 0:
+        krewes[key] = [item]
+      #If period of devo is not created already, make it.
+      else:
+        krewes[key].append(item)
+      #Adds devo name + ducky name in list form to dictionary
+
+except:
+  
+  print("An Error Has Occured")
+      
 def randomizer(dictionary):
   rand_key = random.choice(list(dictionary.keys()))
   rand_val = random.choice(dictionary[rand_key])
@@ -51,4 +57,4 @@ def randomizer(dictionary):
 
 #print(randomizer(dictionary))
 for i in range(100):
-  print(randomizer(dictionary))
+  print(randomizer(krewes))
