99581c9ce75baa4fe60e66e68b7b40a01f93c3ef
RavindraM23 <rmangar30@stuy.edu>
2022-11-30 12:46:55 -0500

    23 mome moment

--- 22_restapiOTHERS/app.py
+++ 22_restapiOTHERS/app.py
@@ -1,7 +1,9 @@
 import json, requests
 
 #response = requests.get("https://api.stuyspec.com/")
-response = requests.get("https://api.stuyspec.com/articles/what-the-duck")
-response = response.json()
+#response = requests.get("https://api.stuyspec.com/articles/what-the-duck")
+#response = response.json()
 
-print(response["preview"])
+response = requests.get("https://api.sunrise-sunset.org/json")
+
+print(response.text)
